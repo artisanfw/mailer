6cecd2c3dfc394dbe60bf08db6fd2c9e91b5288e
Airam Batista <airamb@gmail.com>
2025-06-29 14:36:44 +0100

    Email service and Readme

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # email
-A Singleton-based Email implementation
+A Singleton-based Email implementation for Symfony Mailer.
+
+## Install
+```shell
+  composer require artisanfw/mailer
+```
+
+## Loading the Service
+```php
+$mailerConf = [
+    'dsn' => 'smtp://user:password@smtp.server:port',
+];
+
+$mailer = Email::load($mailerConf);
+```
+
+## Sending an Email
+```php
+Email::i()
+    ->from('test@domain.com')
+    ->to('who@domain.com')
+    ->subject('Welcome to Artisan!')
+    ->html('Send the body msg here!')
+    ->send();
+```
---
+++ src/Email.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Api\Services;
+
+use Symfony\Component\Mailer\Transport;
+use Symfony\Component\Mailer\Mailer;
+use Symfony\Component\Mime\Email as SymfonyEmail;
+
+
+class Email extends SymfonyEmail
+{
+    private const string EMAIL_CONFIG_ERROR = 'Email service requires setup before being used.';
+
+    private static ?Mailer $mailer = null;
+    private static bool $isConfigured = false;
+    private static ?self $instance = null;
+
+    private function __construct()
+    {
+        if (!self::$isConfigured) {
+            throw new \LogicException(self::EMAIL_CONFIG_ERROR);
+        }
+        parent::__construct();
+    }
+
+    public static function i(): self
+    {
+        if (!self::$isConfigured) {
+            throw new \LogicException(self::EMAIL_CONFIG_ERROR);
+        }
+
+        if (self::$instance === null) {
+            self::$instance = new self();
+        }
+
+        return self::$instance;
+    }
+
+    public static function load(array $config): void
+    {
+        if (!self::$isConfigured) {
+            if (!isset($config['dsn'])) {
+                throw new \InvalidArgumentException('"dsn" configuration not found.');
+            }
+            $transport = Transport::fromDsn($config['dsn']);
+            self::$mailer = new Mailer($transport);
+
+            self::$isConfigured = true;
+        }
+    }
+
+    public function send(): void
+    {
+        if (!self::$mailer) {
+            throw new \LogicException(self::EMAIL_CONFIG_ERROR);
+        }
+        self::$mailer->send($this);
+    }
+}
